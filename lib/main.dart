b802537f8c65bce286412440cfd6d95c3f2aef36
WadoodSanjrani <SHERZAAD362@GMAIL.COM>
2026-09-23 23:15:54 +0500

    Fixed login_screen import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'screens/login_screen.dart';   // ← YEH IMPORT ZAROORI HAI
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +23,7 @@ class AdminApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0A1931)),
         useMaterial3: true,
       ),
-      home: LoginScreen(),  // ← const nahi (kyunke LoginScreen const nahi hai)
+      home: const LoginScreen(),
     );
   }
 }
