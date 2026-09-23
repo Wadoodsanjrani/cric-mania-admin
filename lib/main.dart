1e62c2e7ac6cc24e88321c8105b2a8bc01d22a41
WadoodSanjrani <SHERZAAD362@GMAIL.COM>
2026-09-23 23:55:17 +0500

    Fixed const error

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class AdminApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0A1931)),
         useMaterial3: true,
       ),
-      home: LoginScreen(),   // ← sirf const hata diya
+      home: LoginScreen(),
     );
   }
 }
